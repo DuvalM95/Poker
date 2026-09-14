90b61f5751598f416348acf6999adc07320aba96
DuvalM95 <duvalmunoz2@gmail.com>
2026-09-14 12:41:46 -0500

    Permitir dominio de Render en Vite

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: ['poker-pamc.onrender.com'],
   },
   preview: {
     host: true,
     port: 4173,
+    allowedHosts: ['poker-pamc.onrender.com'],
   },
 })
